88a1ee6ba5306ac06b066b14fa2ec91d30b4a588
karoldavid <k.zysk@zoho.com>
2018-02-04 10:55:16 +0100

    chore: watch bundle and restart server when build changes

--- build/bundle.js
+++ build/bundle.js
@@ -7066,8 +7066,8 @@ app.get("/", function (req, res) {
 	res.send(content);
 });
 
-app.listen(3000, function () {
-	console.log("Listening on port 3000");
+app.listen(3333, function () {
+	console.log("Listening on port 3333");
 });
 
 /***/ }),
