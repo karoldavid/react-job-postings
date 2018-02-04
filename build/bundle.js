dc03e05fde9a1f5147fa8168987b643cbca861d2
karoldavid <k.zysk@zoho.com>
2018-02-04 11:41:02 +0100

    feat: wire up server side routing

--- build/bundle.js
+++ build/bundle.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -71,16 +71,22 @@ module.exports = require("react");
 
 /***/ }),
 /* 1 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-router-dom");
+
+/***/ }),
+/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _express = __webpack_require__(2);
+var _express = __webpack_require__(3);
 
 var _express2 = _interopRequireDefault(_express);
 
-var _renderer = __webpack_require__(3);
+var _renderer = __webpack_require__(4);
 
 var _renderer2 = _interopRequireDefault(_renderer);
 
@@ -91,7 +97,7 @@ var app = (0, _express2.default)();
 app.use(_express2.default.static("public"));
 
 app.get("/", function (req, res) {
-	res.send((0, _renderer2.default)());
+	res.send((0, _renderer2.default)(req));
 });
 
 app.listen(3333, function () {
@@ -99,13 +105,13 @@ app.listen(3333, function () {
 });
 
 /***/ }),
-/* 2 */
+/* 3 */
 /***/ (function(module, exports) {
 
 module.exports = require("express");
 
 /***/ }),
-/* 3 */
+/* 4 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -119,28 +125,65 @@ var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _server = __webpack_require__(4);
+var _server = __webpack_require__(5);
 
-var _Home = __webpack_require__(5);
+var _reactRouterDom = __webpack_require__(1);
+
+var _Routes = __webpack_require__(6);
+
+var _Routes2 = _interopRequireDefault(_Routes);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function (req) {
+	var content = (0, _server.renderToString)(_react2.default.createElement(
+		_reactRouterDom.StaticRouter,
+		{ location: req.path, context: {} },
+		_react2.default.createElement(_Routes2.default, null)
+	));
+
+	return "\n\t\t<html>\n\t\t<head></head>\n\t\t<body>\n\t\t\t<div id=\"root\">" + content + "</div>\n\t\t\t<script src=\"bundle.js\"></script>\n\t\t\t</div>\n\t\t</body>\n\t</html>\n\t";
+};
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-dom/server");
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactRouterDom = __webpack_require__(1);
+
+var _Home = __webpack_require__(7);
 
 var _Home2 = _interopRequireDefault(_Home);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = function () {
-	var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
-
-	return "\n\t\t<html>\n\t\t<head></head>\n\t\t<body>\n\t\t\t<div id=\"root\">" + content + "</div>\n\t\t\t<script src=\"bundle.js\"></script>\n\t\t\t</div>\n\t\t</body>\n\t</html>\n\t";
+	return _react2.default.createElement(
+		"div",
+		null,
+		_react2.default.createElement(_reactRouterDom.Route, { exact: true, path: "/", component: _Home2.default })
+	);
 };
 
 /***/ }),
-/* 4 */
-/***/ (function(module, exports) {
-
-module.exports = require("react-dom/server");
-
-/***/ }),
-/* 5 */
+/* 7 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
