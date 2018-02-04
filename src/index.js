36fd45fe066dbf816ff75a6c2443a47f850bc6c6
karoldavid <k.zysk@zoho.com>
2018-02-04 10:55:07 +0100

    chore: listen on port 3333 as per specifications

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ app.get("/", (req, res) => {
 	res.send(content);
 });
 
-app.listen(3000, () => {
-	console.log("Listening on port 3000");
+app.listen(3333, () => {
+	console.log("Listening on port 3333");
 });
